efc673985f94725051ccb1285ab963ad94ac3272
khallihenne <23021@esp.mr>
2025-01-13 14:20:55 +0000

    petit bug

--- projet_SID32/projet_SID32/settings.py
+++ projet_SID32/projet_SID32/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Auth settings
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = '/app/wilaya/'
+LOGOUT_REDIRECT_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- projet_SID32/projet_SID32/urls.py
+++ projet_SID32/projet_SID32/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/login/', permanent=False), name='index'),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('', include('incp.urls'))
+    path('app/', include('incp.urls')),
 ]
